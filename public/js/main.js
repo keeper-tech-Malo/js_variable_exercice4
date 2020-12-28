6938451b33dde17b2a9b0cedac724faf5ec25dbb
Malo <ndekezi37@gmail.com>
2020-12-28 16:48:31 +0100

    fin de l'exercice

---
+++ public/js/main.js
@@ -0,0 +1,25 @@
+// let nom = "N";
+// let prenom = "M";
+// let ville = prompt("quel'est ta ville?",);
+
+// alert (` ${ville}`);
+// console.log(nom,prenom);
+
+// exo 2 
+
+
+let nom = "caliskan";
+let prenom = "ayhan";
+let depart = "bruxelles";
+let destination = "paris";
+let phrase = `Le voyage de ${depart} vers ${destination} a été enregistré au nom de ${nom} ${prenom}`;
+console.log(phrase);
+
+2 b
+let nom = prompt('un nom?');
+let prenom = prompt('un prénom?');
+let depart = prompt('une ville de départ?');
+let destination = prompt('une ville de déstination ?');
+
+let phrase = `Le voyage de ${depart} vers ${destination} a été enregistré au nom de ${nom} ${prenom}`;
+console.log(phrase);
